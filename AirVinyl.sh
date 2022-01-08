b350554dd8c0cc3d9f80a102331e1fedc7f2e89e
nadigo <nadav@goshen.cc>
2022-01-07 20:09:30 -0500

    Update AirVinyl.sh

--- AirVinyl.sh
+++ AirVinyl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#
 # AirVinyl.sh
-# Vinyl to airplay - Stream vinyl from Raspberry Pi to any Airplay (v2) speakers.
+# Vinyl to airplay - Stream vinyl from Raspberry Pi to any Airplay speaker/s.
 
 
 # let's log
@@ -23,7 +22,7 @@ monitorFile=/tmp/monitor.wav
 # verify we have a pipe 
 if [[ ! -p $streamFile ]] ; then mkfifo -m 777 $streamFile ; fi
 # new monitoring file 
-if [[ -f $monitorFile ]] ; then rm $monitorFile ; fi
+#if [[ -f $monitorFile ]] ; then rm $monitorFile ; fi
 # clear pid files 
 rm /tmp/sox.pid 2> /dev/null 
 rm /tmp/raop.pid 2> /dev/null 
@@ -32,27 +31,28 @@ rm /tmp/raop.pid 2> /dev/null
 # start sox recording for sound / silence monitoring
 # make sure default alsa capturing device is 'mixed' with dsnoop for simultaneous access 
 /bin/sox -V2 -q \
--r 48k -b 16 -c 2 -t alsa default \
--r 44100 -b 16 -c 2 -t wav $monitorFile \
-silence 1 0.1 3% -1 3.0 3% &
+-r 48k -b 8 -c 2 -t alsa default \
+-r 44100 -b 8 -c 2 -t wav - \
+silence 1 0.1 1% -1 5.0 5% \
+| cat >> $monitorFile &
 sleep 1
 echo "Starting sox monitoring SOX pid=$! monitorFile=$monitorFile @ $(date)" >> $log 
 
+let file_size=(500)\*1024\*1000
+trim_File () {
+        if [[ $(stat -c %s $1 2>/dev/null) -gt $file_size ]] ; then 
+        truncate -s 1024 $1
+        echo "Flushed $1 @ $(date)" >> $log 
+        fi
+}
 
 while [ true ]; do 
-            until [ "$var1" != "$var2" ]; do
+
+            until [ "$var1" != "$var2" ]; do                                         
+                #truncate 
                 var1=$(stat -c %s $monitorFile)
                 sleep 0.5
                 var2=$(stat -c %s $monitorFile)
-
-                # ToDo need to transact file when it's getting too big
-                
-                #if [[ $(stat -c %s $monitorFile 2>/dev/null) -gt 512000 ]] ; then 
-                #echo '' > $monitorFile ; 
-                #echo "Flushed $monitorFile @ $(date)" >> $log 
-                #var1=$var2+1
-                #sleep 0.5
-                #fi
             done
 
             # sound detected stream from the soundcard input to RAOP player 
@@ -66,6 +66,7 @@ while [ true ]; do
 
 
             until [ "$var1" == "$var2" ]; do
+                trim_File $monitorFile
                 var1=$(stat -c %s $monitorFile)
                 sleep 1
                 var2=$(stat -c %s $monitorFile)
@@ -77,6 +78,3 @@ while [ true ]; do
             echo "Silence Detected killing ROAP pid=$(</tmp/raop.pid) @ $(date)" >> $log 
 
 done
-
-
-
