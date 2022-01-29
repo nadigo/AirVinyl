028687528a2544a50d0e406f6d49b6f58e0aca17
nadigo <nadav@goshen.cc>
2022-01-29 16:17:38 -0500

    Update AirVinyl.sh

--- AirVinyl.sh
+++ AirVinyl.sh
@@ -1,110 +1,110 @@
 #!/bin/bash
-# airvinyl.sh
+# airVinyl.sh
 # Vinyl to airplay - Stream vinyl from Raspberry Pi to any Airplay speaker/s.
+
+## load configuration 
+source airVinyl.conf
+export PID_LIST=""
 
 # let's log
 scriptPath=$(realpath "$0")
 log=$(echo $scriptPath | sed -e 's/\.[^.]*$//').log
-touch $log && echo "Started @ $(date)" > $log
+echo "Started @ $(date)" > $log
 exec > >(tee -a $log) ; exec 2>&1 ; sleep .1
+#exec 2>&1 >> $log
 
-## Load configuration  
-# speakers config  
-source speakers.conf
-
-###
-players=( `echo ${player1[@]}` `echo ${player2[@]}`)
-
-## cleaning things up
-streamPipe=/tmp/phono.pipe
-monitorFile=/tmp/monitor.wav
-monitorFileMax=$(( 500 * 1024 * 1000 ))  #in bytes 
-# new monitoring file
-[[ -f $monitorFile ]] && rm $monitorFile
-touch $monitorFile ; chmod 777 $monitorFile
-# verify we have a pipe
-if [[ ! -p $streamPipe ]] ; then mkfifo -m 777 $streamPipe ; fi
-
-### FUNCTIONS ###
+##   FUNCTIONS   ####  
 trim_File () {
     local trimFile=$1
-    local file_size=$2
-    if [[ $(stat -c %s $trimFile 2>/dev/null) -gt $file_size ]] ; then
+    local maxSize=$2
+    if [[ $(stat -c %s $trimFile 2>/dev/null) -gt $maxSize ]] ; then
     truncate -s 1024 $trimFile
     echo "Flushed $trimFile @ $(date)" >> $log
     fi
 }
 
-player() {  
+player() { 
+
     case $1 in
         'play')
-                # generate ntp file
-                [[ -f $ntpFile ]] &&  rm $ntpFile  
-                $playerCmd -ntp $ntpFile 
+                echo "--> player:play" >> $log 
+                # generate new ntp file
+                [[ -f $STREAMFOLDER/$ntpFile ]] && rm $STREAMFOLDER/$ntpFile  
+                $playerCmd -ntp $STREAMFOLDER/$ntpFile 
                 #wait $!
-                echo "Saved nfp file $ntpFile:$(<$ntpFile) @ $(date)" >> $log
+                echo "Saved nfp file $ntpFile:$(<$STREAMFOLDER/$ntpFile) @ $(date)" >> $log
+                
                 # build pipePlayer 
-                pipePlayerCmd="/bin/sox -V2 -q -r 48k -b 16 -c 2 -t alsa default -r 44100 -b 16 -c 2 -t raw - | tee "
-                for (( i=0; i<${#players[@]} ;i+=2 )); do 
-                    pipePlayerCmd+=">(cat > \$($playerCmd -d 0 -l $latency -p $port -nf $ntpFile -w $wait -v ${players[$i]} ${players[$((i + 1))]} - )) "
+                pipePlayerCmd="stdbuf -i0 -o0 -e0 /bin/sox -V2 -q --ignore-length --buffer $sysLatency -r 48k -b 16 -c 2 -t alsa default -r 44.1k -b 16 -c 2 -t raw - | stdbuf -i0 -o0 -e0 tee "
+                for (( i=0; i<$((${#speakers[@]}-2)) ;i+=2 )); do 
+                    pipePlayerCmd+=">(stdbuf -i0 -o0 -e0  $playerCmd -d 0 -p $port -l $latency -w $wait -nf $STREAMFOLDER/$ntpFile -v ${speakers[$i]} ${speakers[$((i + 1))]} - ) "
                 done
-                pipePlayerCmd+="| cat > /dev/null &"
+                pipePlayerCmd+="| stdbuf -i0 -o0 -e0 $playerCmd -d 0 -p $port -l $latency -w $wait -nf $STREAMFOLDER/$ntpFile -v ${speakers[-2]} ${speakers[-1]} -"
+                
                 # START pipePlayer
-                echo "pipPlayer command: $pipePlayerCmd" >> $log
-                eval $pipePlayerCmd 
-                echo $! > $playerPid 
-                echo "Started pipPlayer pid=$(<$playerPid) @ $(date)" >> $log ;;
-
-        'stop') kill -9 $(<$playerPid) 2> /dev/null 
-                echo "killing player pid=$(<$playerPid)" >> $log ;;
+                echo "pipPlayer CMD:$pipePlayerCmd" >> $log 
+                eval "$rtCMD $pipePlayerCmd > /dev/null &" 
+                echo $! >$STREAMFOLDER/$playerPid
+                PID_LIST+=" $(<$STREAMFOLDER/$playerPid)"
+                echo "Started pipePlayer pid=$(<$STREAMFOLDER/$playerPid) @ $(date)" >> $log ;;
+                
+        'stop') 
+                echo "--> player:stop" >> $log 
+                kill -9 $(<$STREAMFOLDER/$playerPid) 2> /dev/null 
+                echo "killing player pid=$(<$STREAMFOLDER/$playerPid)" >> $log ;;
     esac
 }
 ## END FUNCTIONS ####  
 
 
-# /// ToDo ////
-# build a UI to detect and select Airplay speakers
-# avahi-browse _raop._tcp -rfktp | sed '/\+;wlan/d' | sed '/IPv6/d' | sed -e 's/.*\(;_raop\._tcp;local;\)\(.*\)\(;\).*/\2/'
+#
+#### START ####
+
+## Setting files
+# create stream folder if it dosen't exsist 
+[[ ! -d $STREAMFOLDER ]] && mkdir -p -m 777 $STREAMFOLDER 
+# create pipe if it dosen't exsist 
+[[ ! -p $STREAMFOLDER/$PIPENAME ]] && mkfifo -m 777 $STREAMFOLDER/$PIPENAME
+# create monitorFile if it dosen't exsist 
+[[ -f $STREAMFOLDER/$MONITORFILE ]] && rm $STREAMFOLDER/$MONITORFILE
+touch $STREAMFOLDER/$MONITORFILE ;  chmod 777 $STREAMFOLDER/$MONITORFILE 
 
 
-# start sox recording for sound / silence monitoring
+# start sound / silence monitoring with sox
 # make sure default alsa capturing device is 'mixed' with dsnoop for simultaneous access
 
-# /// ToDo ////
-# find better silence parameters
-# silence 1 0.1 1% -1 5.0 5%
-
-/bin/sox -V2 -q \
--t alsa default \
--t wav - \
-silence 1 0.1 0.1% -1 5.0 -85d \
-| cat >> $monitorFile &
+$rtCMD  /usr/bin/sox \
+	-V2 -q --ignore-length \
+    -t alsa default \
+	-t raw - \
+    silence 1 0.1 0.1% -1 5.0 -85d \
+	| cat >> $STREAMFOLDER/$MONITORFILE &
+PID_LIST+=" $!"
+echo "Starting sox monitoring pid=$! monitorFile=$STREAMFOLDER/$MONITORFILE @ $(date)" >> $log
 sleep 1
-echo "Starting sox monitoring SOX pid=$! monitorFile=$monitorFile @ $(date)" >> $log
-
 
 while [ true ]; do
-    #
+
     until [ "$var1" != "$var2" ]; do
-        var1=$(stat -c %s $monitorFile)
-        sleep 0.5
-        var2=$(stat -c %s $monitorFile)
+        var1=$(stat -c %s $STREAMFOLDER/$MONITORFILE)
+        sleep .5
+        var2=$(stat -c %s $STREAMFOLDER/$MONITORFILE)
     done
-    #
-    # sound detected --> stream to AirPlay
-    echo "Sound Detected @ $(date)" >> $log
+    # sound detected --> player:play
+    echo "Sound Detected starting player @ $(date)" >> $log
     player play
-    #
-    #
+
     until [ "$var1" == "$var2" ]; do
-        trim_File $monitorFile $monitorFileMax
-        var1=$(stat -c %s $monitorFile)
-        sleep 1
-        var2=$(stat -c %s $monitorFile)
+        trim_File $STREAMFOLDER/$MONITORFILE $maxMONITORFILE
+        var1=$(stat -c %s $STREAMFOLDER/$MONITORFILE)
+        sleep .5
+        var2=$(stat -c %s $STREAMFOLDER/$MONITORFILE)
     done
-    #
-    # silence detected --> stop player
-    echo "Silence Detected @ $(date)" >> $log
+    # silence detected --> player:stop
+    echo "Silence Detected stopping player @ $(date)" >> $log
     player stop
-    #
+
 done
+
+trap "kill $PID_LIST" SIGINT
+wait $PID_LIST
